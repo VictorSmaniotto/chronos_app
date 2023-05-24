1485d26961e8f89e27802f8903be322f5b76b527
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-05-24 19:24:48 -0300

    Fiz a appBar

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// Colors
+//Color(0xFF004A8D)
+//Color(0xFF0E6094)
+//Color(0xFF0052A1)
+//Color(0xFF004587)
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0052A1),
+          elevation: 1,
+          title: Image.asset(
+            'assets/logo-branca.png',
+            width: 120,
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'homePage.dart';
+import 'Pages/HomePage.dart';
 
 void main() {
   runApp(const MaterialApp(
